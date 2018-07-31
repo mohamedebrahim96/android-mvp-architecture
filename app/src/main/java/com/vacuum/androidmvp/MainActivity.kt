29f284a57b12bb8e8a7a9a8e1bcca036401f3e46
mohamed ebrahim <ebrahimm131@gmail.com>
2018-07-31 22:27:55 +0200

    start mvp

--- app/src/main/java/com/vacuum/androidmvp/MainActivity.kt
+++ app/src/main/java/com/vacuum/androidmvp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.vacuum.androidmvp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
